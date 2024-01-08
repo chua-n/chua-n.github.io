2fdb75bb317a6f208018b2019d9ed2b0a79c40a4
chuan <chua_n@yeah.net>
2024-01-08 15:46:11 +0800

    调整import语句的顺序

--- _app/src/main.js
+++ _app/src/main.js
@@ -3,10 +3,18 @@ import '/static/css/themes/themes-vue.css'
 import 'docsify-sidebar-collapse/dist/sidebar-folder.min.css'
 import '/static/css/myStyle.css'
 import '/static/css/codeColor.css'
-import 'docsify-edit-on-github'
+import 'docsify-edit-on-github' // 点击编辑就报错
 
 // <body>
 import 'docsify'
+import 'docsify-copy-code' // 复制到剪贴板
+import 'docsify/lib/plugins/search' // 全文搜索 TODO 貌似与数学公式冲突？
+import 'docsify/lib/plugins/zoom-image' // 图片缩放
+import 'docsify/lib/plugins/emoji' // 表情
+import 'docsify-count/dist/countable' // 字数统计
+import 'docsify-pagination/dist/docsify-pagination' // 分页导航插件（上/下章节按钮）
+import 'docsify-scroll-to-top/src/docsify-scroll-to-top' // 回到顶部按钮
+import 'docsify-katex/dist/docsify-katex' // 数学公式
 import '/static/js/docsify-sidebar-collapse'
 // 支持markdown代码段高亮
 import 'prismjs/components/prism-java'
@@ -19,17 +27,9 @@ import 'prismjs/components/prism-json.min'
 import 'prismjs/components/prism-properties.min'
 import 'prismjs/components/prism-http.min'
 import 'prismjs/components/prism-nginx.min'
-
-import 'docsify-copy-code' // 复制到剪贴板
-import 'docsify/lib/plugins/search' // 全文搜索 TODO 貌似与数学公式冲突？
-import 'docsify/lib/plugins/zoom-image' // 图片缩放
-import 'docsify/lib/plugins/emoji' // 表情
-import 'docsify-count/dist/countable' // 字数统计
+// 数学公式
 import 'katex/dist/katex.min'
 import 'marked'
-import 'docsify-katex/dist/docsify-katex'
 import 'katex/dist/katex.min.css'
-import 'docsify-pagination/dist/docsify-pagination' // 分页导航插件（上/下章节按钮）
 // TODO 此处引入 canvas_nest
 import '/static/js/fireworks' // 引入鼠标点击时的烟花效果
-import 'docsify-scroll-to-top/src/docsify-scroll-to-top' // 回到顶部按钮
