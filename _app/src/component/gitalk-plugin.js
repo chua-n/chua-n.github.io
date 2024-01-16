f85c2261dc66287280cb2c6dce70183753c95930
xucy <xucy-e@glodon.com>
2024-01-16 22:41:37 +0800

    测试一下gitalk评论功能

---
+++ _app/src/component/gitalk-plugin.js
@@ -0,0 +1,33 @@
+import Gitalk from 'gitalk';
+
+const gitalk = new Gitalk({
+  clientID: '0677de831ba15682dc35',
+  clientSecret: '25da21a4bba522e21f21c0ddbf0de748336aa086',
+  repo: 'notebook',
+  owner: 'chua-n',
+  admin: ['chua-n'],
+  id: location.hash,
+  distractionFreeMode: false
+});
+
+export default function install(hook) {
+  const dom = Docsify.dom;
+
+  hook.mounted(function (_) {
+    const div = dom.create('div');
+    div.id = 'gitalk-container';
+    const main = dom.getNode('#main');
+    div.style = 'width: ' + (main.clientWidth) + 'px; margin: 0 auto 20px;';
+    dom.appendTo(dom.find('.content'), div);
+  });
+
+  hook.doneEach(function (_) {
+    const el = document.getElementById('gitalk-container');
+    while (el.hasChildNodes()) {
+      el.removeChild(el.firstChild);
+    }
+
+    // eslint-disable-next-line
+    gitalk.render('gitalk-container');
+  });
+}
