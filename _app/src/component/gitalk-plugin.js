646419a94e322101f99e70e5a34172265134e650
chua-n <chua_n@yeah.net>
2024-01-17 06:57:25 +0800

    调一下gitalk设置

--- _app/src/component/gitalk-plugin.js
+++ _app/src/component/gitalk-plugin.js
@@ -1,9 +1,30 @@
 import Gitalk from 'gitalk';
 
+const BASE_CONFIG = {
+  clientID: '0677de831ba15682dc35',
+  clientSecret: '25da21a4bba522e21f21c0ddbf0de748336aa086',
+  repo: 'notebook',
+  owner: 'chua-n',
+  admin: ['chua-n'],
+  labels: ['gitalk'],
+  enableHotKey: true,
+  // createIssueManually: true,
+  distractionFreeMode: false,
+};
+
+function buildGitalkId() {
+  const hash = decodeURI(location.hash);
+  const beginIndex = hash.indexOf('#') + 1;
+  const endIndex = hash.indexOf('?');
+  const res = endIndex > -1 ? hash.substring(beginIndex, endIndex) : hash.substring(beginIndex);
+  console.log('gitalk-id: ', res);
+  return res;
+}
+
 export default function install(hook) {
   const dom = Docsify.dom;
 
-  hook.mounted(function (_) {
+  hook.mounted(() => {
     const div = dom.create('div');
     div.id = 'gitalk-container';
     const main = dom.getNode('#main');
@@ -11,20 +32,15 @@ export default function install(hook) {
     dom.appendTo(dom.find('.content'), div);
   });
 
-  hook.doneEach(function (_) {
+  hook.doneEach(() => {
     const el = document.getElementById('gitalk-container');
     while (el.hasChildNodes()) {
       el.removeChild(el.firstChild);
     }
 
     const gitalk = new Gitalk({
-      clientID: '0677de831ba15682dc35',
-      clientSecret: '25da21a4bba522e21f21c0ddbf0de748336aa086',
-      repo: 'notebook',
-      owner: 'chua-n',
-      admin: ['chua-n'],
-      id: location.hash,
-      distractionFreeMode: false
+      ...BASE_CONFIG,
+      id: buildGitalkId(),
     });
 
     // eslint-disable-next-line
