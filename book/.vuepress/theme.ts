f8a5b1e470f8f13e6ac76ceba6dcb166a0336f94
chua-n <chua_n@yeah.net>
2024-02-20 15:11:23 +0800

    sidebar.ts移入sidebar文件夹, 准备对其进行拆分

--- book/.vuepress/theme.ts
+++ book/.vuepress/theme.ts
@@ -1,6 +1,6 @@
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
-import sidebar from "./sidebar.js";
+import sidebar from "./sidebar";
 import { Page } from "vuepress";
 
 export default hopeTheme({
